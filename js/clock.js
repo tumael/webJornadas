9327b56adade11f5379398629fe9dc24c6055063
jpmejia <cromeocromeo@gmail.com>
2014-09-05 06:35:44 -0400

    modificando columnas de la seccion conferencias

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+$(document).on('ready', iniciarTemporizador);
+
+function iniciarTemporizador(){
+	var fechaFinal = new Date(2014, 09, 07, 12, 30, 00);
+	var fechaHoy = new Date();
+
+	var diferenciaMilisegundos = fechaFinal.getTime() - fechaHoy.getTime();
+	var diferenciaSegundos = (diferenciaMilisegundos/1000);
+
+	var clock = $("#mi-reloj").FlipClock({
+		clockFace: 'DailyCounter'
+	});
+
+	clock.setTime(diferenciaSegundos);
+	clock.setCountdown(true);
+}
